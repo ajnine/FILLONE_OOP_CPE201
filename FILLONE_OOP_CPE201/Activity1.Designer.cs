209968304df61cd79cc06800edd670fb0a0c679f
Ali <Ali@ALI>
2026-01-29 22:00:08 +0800

    Add project files.

---
+++ FILLONE_OOP_CPE201/Activity1.Designer.cs
@@ -0,0 +1,360 @@
+﻿namespace FILLONE_OOP_CPE201
+{
+    partial class Activity1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Activity1));
+            pictureBox1 = new PictureBox();
+            pictureBox2 = new PictureBox();
+            pictureBox3 = new PictureBox();
+            pictureBox4 = new PictureBox();
+            pictureBox5 = new PictureBox();
+            pictureBox6 = new PictureBox();
+            pictureBox7 = new PictureBox();
+            pictureBox8 = new PictureBox();
+            pictureBox9 = new PictureBox();
+            pictureBox10 = new PictureBox();
+            pictureBox11 = new PictureBox();
+            pictureBox12 = new PictureBox();
+            pictureBox13 = new PictureBox();
+            pictureBox14 = new PictureBox();
+            pictureBox15 = new PictureBox();
+            label1 = new Label();
+            priceTxtbox = new TextBox();
+            label2 = new Label();
+            itemnameTxtbox = new TextBox();
+            newBtn = new Button();
+            exitBtn = new Button();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox3).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox4).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox5).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox6).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox7).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox8).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox9).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox10).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox11).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox12).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox13).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox14).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox15).BeginInit();
+            SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Image = (Image)resources.GetObject("pictureBox1.Image");
+            pictureBox1.Location = new Point(12, 12);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(158, 134);
+            pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox1.TabIndex = 0;
+            pictureBox1.TabStop = false;
+            pictureBox1.Click += pictureBox1_Click;
+            // 
+            // pictureBox2
+            // 
+            pictureBox2.Image = (Image)resources.GetObject("pictureBox2.Image");
+            pictureBox2.Location = new Point(176, 12);
+            pictureBox2.Name = "pictureBox2";
+            pictureBox2.Size = new Size(158, 134);
+            pictureBox2.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox2.TabIndex = 1;
+            pictureBox2.TabStop = false;
+            pictureBox2.Click += pictureBox2_Click;
+            // 
+            // pictureBox3
+            // 
+            pictureBox3.Image = (Image)resources.GetObject("pictureBox3.Image");
+            pictureBox3.Location = new Point(340, 12);
+            pictureBox3.Name = "pictureBox3";
+            pictureBox3.Size = new Size(158, 134);
+            pictureBox3.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox3.TabIndex = 2;
+            pictureBox3.TabStop = false;
+            pictureBox3.Click += pictureBox3_Click;
+            // 
+            // pictureBox4
+            // 
+            pictureBox4.Image = (Image)resources.GetObject("pictureBox4.Image");
+            pictureBox4.Location = new Point(504, 12);
+            pictureBox4.Name = "pictureBox4";
+            pictureBox4.Size = new Size(158, 134);
+            pictureBox4.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox4.TabIndex = 3;
+            pictureBox4.TabStop = false;
+            pictureBox4.Click += pictureBox4_Click;
+            // 
+            // pictureBox5
+            // 
+            pictureBox5.Image = (Image)resources.GetObject("pictureBox5.Image");
+            pictureBox5.Location = new Point(668, 12);
+            pictureBox5.Name = "pictureBox5";
+            pictureBox5.Size = new Size(158, 134);
+            pictureBox5.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox5.TabIndex = 4;
+            pictureBox5.TabStop = false;
+            pictureBox5.Click += pictureBox5_Click;
+            // 
+            // pictureBox6
+            // 
+            pictureBox6.Image = (Image)resources.GetObject("pictureBox6.Image");
+            pictureBox6.Location = new Point(668, 146);
+            pictureBox6.Name = "pictureBox6";
+            pictureBox6.Size = new Size(158, 134);
+            pictureBox6.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox6.TabIndex = 9;
+            pictureBox6.TabStop = false;
+            pictureBox6.Click += pictureBox6_Click;
+            // 
+            // pictureBox7
+            // 
+            pictureBox7.Image = (Image)resources.GetObject("pictureBox7.Image");
+            pictureBox7.Location = new Point(504, 146);
+            pictureBox7.Name = "pictureBox7";
+            pictureBox7.Size = new Size(158, 134);
+            pictureBox7.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox7.TabIndex = 8;
+            pictureBox7.TabStop = false;
+            pictureBox7.Click += pictureBox7_Click;
+            // 
+            // pictureBox8
+            // 
+            pictureBox8.Image = (Image)resources.GetObject("pictureBox8.Image");
+            pictureBox8.Location = new Point(340, 146);
+            pictureBox8.Name = "pictureBox8";
+            pictureBox8.Size = new Size(158, 134);
+            pictureBox8.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox8.TabIndex = 7;
+            pictureBox8.TabStop = false;
+            pictureBox8.Click += pictureBox8_Click;
+            // 
+            // pictureBox9
+            // 
+            pictureBox9.Image = (Image)resources.GetObject("pictureBox9.Image");
+            pictureBox9.Location = new Point(176, 146);
+            pictureBox9.Name = "pictureBox9";
+            pictureBox9.Size = new Size(158, 134);
+            pictureBox9.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox9.TabIndex = 6;
+            pictureBox9.TabStop = false;
+            pictureBox9.Click += pictureBox9_Click;
+            // 
+            // pictureBox10
+            // 
+            pictureBox10.Image = (Image)resources.GetObject("pictureBox10.Image");
+            pictureBox10.Location = new Point(12, 146);
+            pictureBox10.Name = "pictureBox10";
+            pictureBox10.Size = new Size(158, 134);
+            pictureBox10.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox10.TabIndex = 5;
+            pictureBox10.TabStop = false;
+            pictureBox10.Click += pictureBox10_Click;
+            // 
+            // pictureBox11
+            // 
+            pictureBox11.Image = (Image)resources.GetObject("pictureBox11.Image");
+            pictureBox11.Location = new Point(668, 280);
+            pictureBox11.Name = "pictureBox11";
+            pictureBox11.Size = new Size(158, 134);
+            pictureBox11.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox11.TabIndex = 14;
+            pictureBox11.TabStop = false;
+            pictureBox11.Click += pictureBox11_Click;
+            // 
+            // pictureBox12
+            // 
+            pictureBox12.Image = (Image)resources.GetObject("pictureBox12.Image");
+            pictureBox12.Location = new Point(504, 280);
+            pictureBox12.Name = "pictureBox12";
+            pictureBox12.Size = new Size(158, 134);
+            pictureBox12.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox12.TabIndex = 13;
+            pictureBox12.TabStop = false;
+            pictureBox12.Click += pictureBox12_Click;
+            // 
+            // pictureBox13
+            // 
+            pictureBox13.Image = (Image)resources.GetObject("pictureBox13.Image");
+            pictureBox13.Location = new Point(340, 280);
+            pictureBox13.Name = "pictureBox13";
+            pictureBox13.Size = new Size(158, 134);
+            pictureBox13.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox13.TabIndex = 12;
+            pictureBox13.TabStop = false;
+            pictureBox13.Click += pictureBox13_Click;
+            // 
+            // pictureBox14
+            // 
+            pictureBox14.Image = (Image)resources.GetObject("pictureBox14.Image");
+            pictureBox14.Location = new Point(176, 280);
+            pictureBox14.Name = "pictureBox14";
+            pictureBox14.Size = new Size(158, 134);
+            pictureBox14.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox14.TabIndex = 11;
+            pictureBox14.TabStop = false;
+            pictureBox14.Click += pictureBox14_Click;
+            // 
+            // pictureBox15
+            // 
+            pictureBox15.Image = (Image)resources.GetObject("pictureBox15.Image");
+            pictureBox15.Location = new Point(12, 280);
+            pictureBox15.Name = "pictureBox15";
+            pictureBox15.Size = new Size(158, 134);
+            pictureBox15.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox15.TabIndex = 10;
+            pictureBox15.TabStop = false;
+            pictureBox15.Click += pictureBox15_Click;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(16, 449);
+            label1.Name = "label1";
+            label1.Size = new Size(86, 20);
+            label1.TabIndex = 15;
+            label1.Text = "Item Name:";
+            label1.Click += label1_Click;
+            // 
+            // priceTxtbox
+            // 
+            priceTxtbox.Location = new Point(471, 442);
+            priceTxtbox.Name = "priceTxtbox";
+            priceTxtbox.Size = new Size(135, 27);
+            priceTxtbox.TabIndex = 18;
+            priceTxtbox.TextChanged += textBox2_TextChanged;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(421, 445);
+            label2.Name = "label2";
+            label2.Size = new Size(44, 20);
+            label2.TabIndex = 17;
+            label2.Text = "Price:";
+            // 
+            // itemnameTxtbox
+            // 
+            itemnameTxtbox.Location = new Point(108, 446);
+            itemnameTxtbox.Name = "itemnameTxtbox";
+            itemnameTxtbox.Size = new Size(307, 27);
+            itemnameTxtbox.TabIndex = 19;
+            // 
+            // newBtn
+            // 
+            newBtn.Location = new Point(623, 440);
+            newBtn.Name = "newBtn";
+            newBtn.Size = new Size(94, 29);
+            newBtn.TabIndex = 20;
+            newBtn.Text = "New";
+            newBtn.UseVisualStyleBackColor = true;
+            newBtn.Click += newBtn_Click;
+            // 
+            // exitBtn
+            // 
+            exitBtn.Location = new Point(723, 440);
+            exitBtn.Name = "exitBtn";
+            exitBtn.Size = new Size(94, 29);
+            exitBtn.TabIndex = 21;
+            exitBtn.Text = "Exit";
+            exitBtn.UseVisualStyleBackColor = true;
+            exitBtn.Click += exitBtn_Click;
+            // 
+            // Activity1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(842, 500);
+            Controls.Add(exitBtn);
+            Controls.Add(newBtn);
+            Controls.Add(itemnameTxtbox);
+            Controls.Add(priceTxtbox);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(pictureBox11);
+            Controls.Add(pictureBox12);
+            Controls.Add(pictureBox13);
+            Controls.Add(pictureBox14);
+            Controls.Add(pictureBox15);
+            Controls.Add(pictureBox6);
+            Controls.Add(pictureBox7);
+            Controls.Add(pictureBox8);
+            Controls.Add(pictureBox9);
+            Controls.Add(pictureBox10);
+            Controls.Add(pictureBox5);
+            Controls.Add(pictureBox4);
+            Controls.Add(pictureBox3);
+            Controls.Add(pictureBox2);
+            Controls.Add(pictureBox1);
+            Name = "Activity1";
+            Text = "Example 1";
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox3).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox4).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox5).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox6).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox7).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox8).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox9).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox10).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox11).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox12).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox13).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox14).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox15).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private PictureBox pictureBox1;
+        private PictureBox pictureBox2;
+        private PictureBox pictureBox3;
+        private PictureBox pictureBox4;
+        private PictureBox pictureBox5;
+        private PictureBox pictureBox6;
+        private PictureBox pictureBox7;
+        private PictureBox pictureBox8;
+        private PictureBox pictureBox9;
+        private PictureBox pictureBox10;
+        private PictureBox pictureBox11;
+        private PictureBox pictureBox12;
+        private PictureBox pictureBox13;
+        private PictureBox pictureBox14;
+        private PictureBox pictureBox15;
+        private Label label1;
+        private TextBox priceTxtbox;
+        private Label label2;
+        private TextBox itemnameTxtbox;
+        private Button newBtn;
+        private Button exitBtn;
+    }
+}
---
+++ FILLONE_OOP_CPE201/Activity1.cs
@@ -0,0 +1,121 @@
+namespace FILLONE_OOP_CPE201
+{
+    public partial class Activity1 : Form
+    {
+        public Activity1()
+        {
+            InitializeComponent();
+        }
+
+        private void pictureBox2_Click(object sender, EventArgs e)
+        {
+            itemnameTxtbox.Text = "1 – pc. Chickenjoy Solo";
+            priceTxtbox.Text = "85";
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox11_Click(object sender, EventArgs e)
+        {
+            itemnameTxtbox.Text = "1 – pc. Burger Steak With Drink";
+            priceTxtbox.Text = "84";
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            itemnameTxtbox.Text = "Supermeal w/ Drink";
+            priceTxtbox.Text = "224";
+        }
+
+        private void pictureBox3_Click(object sender, EventArgs e)
+        {
+            itemnameTxtbox.Text = "1-pc. Chickenjoy w/ Coke Float";
+            priceTxtbox.Text = "134";
+        }
+
+        private void pictureBox4_Click(object sender, EventArgs e)
+        {
+            itemnameTxtbox.Text = "1 – pc. Chickenjoy With Drink";
+            priceTxtbox.Text = "105";
+        }
+
+        private void pictureBox5_Click(object sender, EventArgs e)
+        {
+            itemnameTxtbox.Text = "1 – pc. Chickenjoy With Jolly Spaghetti Solo";
+            priceTxtbox.Text = "135";
+        }
+
+        private void pictureBox10_Click(object sender, EventArgs e)
+        {
+            itemnameTxtbox.Text = "1 – pc. Chickenjoy W/ Jolly Spaghetti w/ Drink";
+            priceTxtbox.Text = "172";
+        }
+
+        private void pictureBox9_Click(object sender, EventArgs e)
+        {
+            itemnameTxtbox.Text = "2 – pc. Pancakes Solo";
+            priceTxtbox.Text = "87";
+        }
+
+        private void pictureBox8_Click(object sender, EventArgs e)
+        {
+            itemnameTxtbox.Text = "6 – pc. Chickenjoy Bucket w/ Jolly Spaghetti";
+            priceTxtbox.Text = "697";
+        }
+
+        private void pictureBox7_Click(object sender, EventArgs e)
+        {
+            itemnameTxtbox.Text = "6 – pc. Chickenjoy Solo";
+            priceTxtbox.Text = "467";
+        }
+
+        private void pictureBox6_Click(object sender, EventArgs e)
+        {
+            itemnameTxtbox.Text = "6 – pc. Chickenjoy with Palabok Family Pan";
+            priceTxtbox.Text = "927";
+        }
+
+        private void pictureBox15_Click(object sender, EventArgs e)
+        {
+            itemnameTxtbox.Text = "Amazing Aloha Champ Jr., Fries & Drink";
+            priceTxtbox.Text = "221";
+        }
+
+        private void pictureBox14_Click(object sender, EventArgs e)
+        {
+            itemnameTxtbox.Text = "Chickenjoy Bucket Family Meals";
+            priceTxtbox.Text = "745";
+        }
+
+        private void pictureBox13_Click(object sender, EventArgs e)
+        {
+            itemnameTxtbox.Text = "Jolly Crispy Fries Bucket";
+            priceTxtbox.Text = "191";
+        }
+
+        private void pictureBox12_Click(object sender, EventArgs e)
+        {
+            itemnameTxtbox.Text = "Double Cheesy Yumburger With Fries & Drink";
+            priceTxtbox.Text = "217";
+        }
+
+        private void newBtn_Click(object sender, EventArgs e)
+        {
+            itemnameTxtbox.Clear();
+            priceTxtbox.Clear();
+        }
+
+        private void exitBtn_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
